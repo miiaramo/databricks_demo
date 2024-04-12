3a2a7dd9918690006d8e27f20290c3c37482019d
miiaramo <miiaramo>
2024-04-12 08:54:41 +0000

    New version of hello

--- hello.py
+++ hello.py
@@ -1,12 +1,46 @@
+# Databricks notebook source
 print("hello")
 
-# Multi language - Use Python, Scala, SQL, and R, all in same notebook (demo: magic commands)
-# Collaborative - Real-time co-precense and co-editing (demo: give access)
-# Explorative - Built in charts and data profiles 
-# Adabtable - Install packages and import modules (demo: install package)
-# Reproducable - track changes and use Git version control (demo: history)
-# Get to production faster - Schedule noteboooks as a jobs or build a dashboard from their results in the notebook
-# Enterprise ready - Estreprise-grade access controls, identity management, and auditability
-
 # dbutils 
+dbutils.widgets.text("greeting", "hello")
 
+# COMMAND ----------
+
+dbutils.widgets.get("greeting")
+
+# COMMAND ----------
+
+# Multi language - Use Python, Scala, SQL, and R, all in same notebook (demo: magic commands)
+
+# COMMAND ----------
+
+# Collaborative - Real-time co-precense and co-editing (demo: give access)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- Explorative - Built in charts and data profiles (demo: filter results)
+# MAGIC
+# MAGIC SELECT 'hello' as greeting
+# MAGIC UNION
+# MAGIC SELECT 'moi' as greeting
+# MAGIC UNION
+# MAGIC SELECT 'hello, world' as greeting
+# MAGIC UNION
+# MAGIC SELECT 'hallo' as greeting
+
+# COMMAND ----------
+
+# Adabtable - Install packages and import modules (demo: install package)
+
+# COMMAND ----------
+
+# Reproducable - track changes and use Git version control (demo: history)
+
+# COMMAND ----------
+
+# Get to production faster - Schedule noteboooks as a jobs or build a dashboard from their results in the notebook
+
+# COMMAND ----------
+
+# Enterprise ready - Estreprise-grade access controls, identity management, and auditability
