628a09ae98484fe23327ec3eeb193986e9fd6ca9
Miia Rämö <13770384+miiaramo@users.noreply.github.com>
2024-04-12 10:30:40 +0200

    What to cover

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-# Databricks notebook source
 print("hello")
+
+# Multi language - Use Python, Scala, SQL, and R, all in same notebook (demo: magic commands)
+# Collaborative - Real-time co-precense and co-editing (demo: give access)
+# Explorative - Built in charts and data profiles 
+# Adabtable - Install packages and import modules (demo: install package)
+# Reproducable - track changes and use Git version control (demo: history)
+# Get to production faster - Schedule noteboooks as a jobs or build a dashboard from their results in the notebook
+# Enterprise ready - Estreprise-grade access controls, identity management, and auditability
+
+# dbutils 
+
